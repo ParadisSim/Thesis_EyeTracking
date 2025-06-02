203abe7ec922e99a322053ca0d44986a5eb335db
ParadisSim <simon.paradis@uqtr.ca>
2025-06-02 11:23:55 -0500

    processing, investigation of uneven samples, plot by emotion and face int

--- Script_eyet.R
+++ Script_eyet.R
@@ -8,6 +8,8 @@ library(saccades)
 #install.packages('eyetrackingR')
 library(eyetrackingR)
 library(ggplot2)
+install.packages('cowplot')
+library(cowplot)
 
 data <- analyse_prelim_mai_2025
 data_for_condi <- data %>% mutate(condi = paste(voice_int, face_int, emotion))
@@ -15,12 +17,48 @@ data_for_condi <- data %>% mutate(condi = paste(voice_int, face_int, emotion))
 #Dont know why but some current objects where NA ? Maybe pauses ?
 data_for_condi <- data_for_condi %>% filter(!is.na(CurrentObject)) %>% filter(CurrentObject == "Stimulus")
 
-data_rephasetime <- data_for_condi %>%
-  group_by(condi) %>%
+
+
+vef <- data_for_condi %>% filter(Subject == 1,Session == 2, TrialId == 221)
+#Might be because we restarted and called it session 2 but the restart number might have been at trial 221. Did we keep both ?
+#to sort
+vefx <- data_for_condi %>% group_by(Subject,Session) %>% summarise(allo = length(unique(TrialId)))
+vefx %>% filter(allo != 560)
+
+#P9 prob 1 trial de trop
+vef9 <-  data_for_condi %>% group_by(Subject,Session) %>% filter(Subject == 9) %>% summarise(allo = length(unique(TrialId)))
+vef9
+#1121
+
+#vef 14
+vef14 <-  data_for_condi %>% group_by(Subject,Session) %>% filter(Subject == 14) %>% summarise(allo = length(unique(TrialId)))
+vef14
+#1356 ?
+
+#vef 21
+vef21 <-  data_for_condi %>% group_by(Subject,Session) %>% filter(Subject == 21) %>% summarise(allo = length(unique(TrialId)))
+vef21
+#1120 ok ?
+
+#vef 25
+vef25 <-  data_for_condi %>% group_by(Subject,Session) %>% filter(Subject == 25) %>% summarise(allo = length(unique(TrialId)))
+vef25
+#2 bons folder de 560, mais un de 404 ? lol
+
+
+
+data_processed_no_weird <- data_for_condi %>% filter(Subject !=1,Subject != 9,Subject != 14, Subject != 18 ,Subject != 19, Subject != 21, Subject != 22, Subject != 25)
+
+
+data_rephasetime <- data_processed_no_weird %>% mutate(unique_trial = as.numeric(TrialId) + 560*(as.numeric(Session)-1))
+
+data_rephasetime <- data_rephasetime %>%
+  group_by(Subject,Session, condi,voiceTime,voiceID,faceID,lateralisation,unique_trial) %>%
   mutate(time_rebased = RTTime - first(RTTime)) %>%
   ungroup()
 
-
+pardon <- data_rephasetime %>% filter(time_rebased < 0)
+#not working for p1 ?
 
 #---------------------------- SACCADES PACKAGE ----------------
 #Analyses are threefold. One with silence, one with neutral voices and one with emotional voices. 
@@ -90,7 +128,8 @@ ok
 #Now, since the package cannot handle ++ subjects and condition, we will test other methods before trying to revert find what were condition and participant numbers.
 
 
-#----------------- Eyetracking R------------------
+#----------------- Eyetracking R full data ------------------
+  #Note : Need to find out issues with rep per cond with some participants and the window time frame
 
 #Validity right et left - 0 c'est bon. Si jamais trop peu de données ainsi, voir : https://researchwiki.solo.universiteitleiden.nl/xwiki/wiki/researchwiki.solo.universiteitleiden.nl/download/Software/E-Prime/E-Prime%20and%20Tobii/WebHome/eet_2_user_guide14052019.pdf?rev=1.1
 #Faire validity right == 0 & validity left == 0
@@ -106,24 +145,15 @@ data_trackloss %>% filter(TrackL == TRUE)
 data_processed <- data_trackloss %>% mutate(Neutral = AOIStimulus == "Neutral", Emotion = AOIStimulus == "Emotion")
 
 
-
 unique(data_processed %>% filter(Session != 1 & Session !=2) %>% select(Subject, Session))
 #Make trials go from 1 to 1120 for each part.
 #Lets temprarily not fix and just not analyse those
 
 #9 = 1121, 14,18,19,21,22,25 on plusieurs restart a fix
-data_processed_no_weird <- data_processed %>% filter(Subject != 9,Subject != 14, Subject != 18 ,Subject != 19, Subject != 21, Subject != 22, Subject != 25)
-
-data_p <- data_processed_no_weird %>% mutate(unique_trial = as.numeric(TrialId) + 560*(as.numeric(Session)-1))
-
-data_p %>% group_by(Subject,TrialId) %>% summarise(count = length(unique(TrialId))) %>% summary()
-data_p %>% group_by(Subject,unique_trial) %>% summarise(count = length(unique(unique_trial))) %>% summary()
 
 #Time column doit être from trial onset. Donc on devrait prendre le temps à la première mention de fixation et à la dernière de 0 à x...
 
-
-
-data_eye <- make_eyetrackingr_data(data_p, 
+data_eye <- make_eyetrackingr_data(data_processed, 
                                participant_column = "Subject",
                                trial_column =  "unique_trial",
                                time_column = "time_rebased",
@@ -131,13 +161,6 @@ data_eye <- make_eyetrackingr_data(data_p,
                                aoi_columns = c('Neutral','Emotion'),
                                treat_non_aoi_looks_as_missing = TRUE)
 
-data_eye2 <- make_eyetrackingr_data(data_p, 
-                                   participant_column = "Subject",
-                                   trial_column =  "unique_trial",
-                                   time_column = "time_rebased",
-                                   trackloss_column = "TrackL",
-                                   aoi_columns = c('Neutral','Emotion'),
-                                   treat_non_aoi_looks_as_missing = FALSE)
 
 
 response_window_eye <- subset_by_window(data_eye, 
@@ -146,9 +169,7 @@ response_window_eye <- subset_by_window(data_eye,
                                     rezero = FALSE)
 
 
-
 response_window_clean <- clean_by_trackloss(data = response_window_eye, trial_prop_thresh = .25)
-
 
 response_time <- make_time_sequence_data(response_window_clean, time_bin_size = 50,
                                          predictor_columns = c("emotion"),
@@ -161,31 +182,84 @@ plot(response_time, predictor_column = "emotion") +
   coord_cartesian(ylim = c(0,1))
 
 
+data_eye2 <- make_eyetrackingr_data(data_processed, 
+                                   participant_column = "Subject",
+                                   trial_column =  "unique_trial",
+                                   time_column = "time_rebased",
+                                   trackloss_column = "TrackL",
+                                   aoi_columns = c('Neutral','Emotion'),
+                                   treat_non_aoi_looks_as_missing = FALSE)
 
-#Visualize baseline
-response_time <- make_time_sequence_data(response_window_clean, time_bin_size = 100, 
+response_window_eye2 <- subset_by_window(data_eye2, 
+                                        window_start_time = 0,
+                                        window_end_time = 2000,
+                                        rezero = FALSE)
+
+
+response_window_clean2 <- clean_by_trackloss(data = response_window_eye2, trial_prop_thresh = .25)
+
+
+
+response_time2 <- make_time_sequence_data(response_window_clean2, time_bin_size = 50,
+                                         predictor_columns = c("emotion"),
                                          aois = "Emotion"
 )
 
 # visualize time results
-plot(response_time) + 
+plot(response_time2, predictor_column = "emotion") + 
   theme_light() +
   coord_cartesian(ylim = c(0,1))
 
-factor(response_window_clean$lateralisation)
-response_window_clean <- response_window_clean %>% mutate(int_f = factor(face_int))
 
 
 
-response_time2 <- make_time_sequence_data(response_window_clean, time_bin_size = 50, 
-                                         predictor_columns = c("int_f"),
+factor(response_window_clean2$lateralisation)
+
+response_window_cleanx <- response_window_clean2 %>% mutate(int_f = factor(face_int),int_v = factor(voice_int, levels =c("ence", "0", "20", "40", "60", "80", "100"))) 
+
+
+
+response_timez <- make_time_sequence_data(response_window_cleanx, time_bin_size = 50, 
+                                         predictor_columns = c("int_f", "emotion"),
                                          aois = "Emotion"
 )
 
 # visualize time results
-plot(response_time2, predictor_column = "int_f") + 
+plot(response_timez, predictor_column = c("int_f")) + 
   theme_light() +
   coord_cartesian(ylim = c(0,1))
+
+
+
+# visualize time results
+plot(response_time2, predictor_column = "int_v") + 
+  theme_light() +
+  coord_cartesian(ylim = c(0,1))
+
+
+
+response_time_em <- make_time_sequence_data(response_window_cleanx, time_bin_size = 50, 
+                                          predictor_columns = c("int_v","emotion"),
+                                          aois = "Emotion"
+)
+ang <- plot(response_time_em %>% filter(emotion == "A"), predictor_column = "int_v") + 
+  theme_light() +
+  coord_cartesian(ylim = c(0,1))
+
+fear <- plot(response_time_em %>% filter(emotion == "F"), predictor_column = "int_v") + 
+  theme_light() +
+  coord_cartesian(ylim = c(0,1))
+
+hap <- plot(response_time_em %>% filter(emotion == "H"), predictor_column = "int_v") + 
+  theme_light() +
+  coord_cartesian(ylim = c(0,1))
+
+sad <- plot(response_time_em %>% filter(emotion == "S"), predictor_column = "int_v") + 
+  theme_light() +
+  coord_cartesian(ylim = c(0,1))
+
+plot_grid(ang,fear,hap,sad,labels = c('Anger', 'Fear','Happiness','Sadness'),label_size =  7)
+
 
 
 
